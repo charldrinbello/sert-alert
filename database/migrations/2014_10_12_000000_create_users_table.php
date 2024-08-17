e89305a0b32d00331d8ae8353a9d4eedcd45e933
charldrinbello <charldrinbello@gmail.com>
2024-08-17 20:56:07 +0800

    fix error in update profile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('position'); 
-            $table->string('course_year'); 
+            $table->string('course_section'); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -30,7 +30,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        $table->dropColumn('position');
-        $table->dropColumn('course_year');
     }
 };
+
